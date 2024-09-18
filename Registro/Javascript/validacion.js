497a5b7d7b7f632d1d609135638320b00a5e0e48
Kata-design97 <Katherine.guevara97@gmail.com>
2024-09-18 00:13:44 -0600

    elimine carpetas
    elimine las carpetas de login y register

--- Registro/Javascript/validacion.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
-    var password = document.getElementById('password').value;
-    
-    if (password.length < 6) {
-      alert('La contraseña debe tener al menos 6 caracteres.');
-      event.preventDefault();
-    }
-  });
-  
